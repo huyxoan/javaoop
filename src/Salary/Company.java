2defca895a171b411aa79fee40db8631495b924c
Huy Xoan <huyxoan@gmail.com>
2020-10-24 20:52:10 +0700

    Fix review

--- src/Salary/Company.java
+++ src/Salary/Company.java
@@ -8,48 +8,34 @@ public class Company {
     public Company(){
 
     }
-    public Company(String nameofCompany, String nameofmanager, String listofemployee){
 
+    public Company(String nameofCompany, String nameofmanager, String listofemployee){
         this.NameofCompany = nameofCompany;
         this.Nameofmanager = nameofmanager;
         this.Listofemployee = listofemployee;
-
     }
 
     public String getNameofCompany() {
-
         return NameofCompany;
-
     }
 
     public void setNameofCompany(String nameofCompany) {
-
         this.NameofCompany = nameofCompany;
-
     }
 
-    public String getNameofmanager()
-
-    {
+    public String getNameofmanager() {
         return Nameofmanager;
     }
 
     public void setNameofmanager(String nameofmanager) {
-
         this.Nameofmanager = nameofmanager;
-
     }
 
     public String getListofemployee() {
-
         return Listofemployee;
-
     }
 
-    public void setListofemployee(String listofemployee)
-
-    {
+    public void setListofemployee(String listofemployee) {
         this.Listofemployee = listofemployee;
     }
-
 }
